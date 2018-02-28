238550559461b3671f1fe88bd1be8b878ab2b00d
Pratik <34038791+Pratik3199@users.noreply.github.com>
2018-02-28 11:25:36 +0530

    Create Exceed.java

---
+++ Exceed.java
@@ -0,0 +1,63 @@
+package Exceed.java;
+
+public class ExceedLimitsPuzzle {
+
+	public static void main(String[] args) {
+		short counter = 1;
+		float sum = 0;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "   " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+		
+		counter++;
+		sum = sum + 0.1F;
+		System.out.println(counter + "  " + sum);
+	}
+}
+ 
